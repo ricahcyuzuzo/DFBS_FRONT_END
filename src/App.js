f1aabc70b567788783f62c2840b9ee7b12c8e2bf
JoelKwihangana <joelkwijoe@gmail.com>
2022-09-01 22:03:04 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/header/Header";
 import NavbarHome from "./components/Navbar/NavbarHome";
 
 function App() {
   return (
     <div className="App">
       <NavbarHome />
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+  return (
+    <>
+      <div className="container-fluid header">
+        <div className="row ">
+          <div className="col col-4 ">
+            <h3 className="mb-4">Digital Forex Bureau System</h3>
+            <p className="fs-6">
+              Find the solution of the problems in exchanging currency and
+              transferring money digitally to meet your needs and satisfaction.
+            </p>
+            <button className="btn exchangebtn fs-5">EXCHANGE NOW</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
