a007dbd37cb3a1802635c0e442bda29bf4436962
JoelKwihangana <joelkwijoe@gmail.com>
2022-09-04 07:40:39 +0200

    admin

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Content from "./components/Content";
-import SideBar from "./components/SideBar";
+import Content from "./Content";
+import SideBar from "./SideBar";
 
 const Admin = () => {
   return (
