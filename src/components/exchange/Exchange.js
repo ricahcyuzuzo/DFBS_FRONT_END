b01df5b6f5065ecebc2916a18d6dc5e13399d7d3
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-09-06 17:38:25 +0200

    payment fix

--- src/components/exchange/Exchange.js
+++ src/components/exchange/Exchange.js
@@ -3,17 +3,24 @@ import "../styles/exchange.scss";
 import HomeHeader from "../Headers/HomeHeader";
 import Footer from "../Footer/Footer";
 import { backendUrl } from "../../constansts";
-import { errorHandler } from "../../helpers";
+import { errorHandler, toastMessage } from "../../helpers";
 import { setCurrencies } from "../../actions/currencies";
 import { useDispatch, useSelector } from "react-redux";
+import { useFlutterwave, closePaymentModal } from "flutterwave-react-v3";
 import axios from "axios";
+import Loader from "../Loader/Loader";
 function Exchange() {
+  const { email, fullName, phone, role, token, address } = useSelector(
+    (state) => state.user
+  );
   const dispatch = useDispatch();
   const { currencies } = useSelector((state) => state.currencies);
   const [amountToGet, setAmountToGet] = useState(0);
   const [amount, setAmount] = useState(1);
   const [fromCurrency, setFromCurrency] = useState("");
   const [toCurrency, setToCurrenyc] = useState("");
+  const [phoneOrAccount, setPhoneOrAccount] = useState("");
+  const [showLoader, setShowLoader] = useState(false);
 
   const fetchCurrencies = () => {
     axios
@@ -38,7 +45,7 @@ function Exchange() {
     // const from = currencies.find((item) => item.code == fromCurrency);
     const to = currencies.find((item) => item.code == toCurrency);
     console.log("to", to);
-    setAmountToGet(amount / to?.sellPrice);
+    setAmountToGet((amount / to?.sellPrice).toFixed(2));
   };
 
   function compare(a, b) {
@@ -50,6 +57,73 @@ function Exchange() {
     }
     return 0;
   }
+
+  const config = {
+    public_key: "FLWPUBK-0cfa7cdb33ef479ec93b717156b7b779-X",
+    tx_ref: Date.now(),
+    amount: amount,
+    currency: "RWF",
+    payment_options: "card,mobilemoney",
+    customer: {
+      email,
+      phonenumber: "0" + phone,
+      name: fullName,
+    },
+    customizations: {
+      title: "Exchange Berau - Checkout Page",
+      description: "",
+      logo: process.env.REACT_APP_PROJECT_LOGO,
+    },
+  };
+  const handleFlutterPayment = useFlutterwave(config);
+
+  const handlePay = () => {
+    if (email == "") {
+      toastMessage("error", "You must be logged in, inorder to exchange ");
+    } else {
+      if (amount.trim() === "" || phoneOrAccount === "") {
+        toastMessage("error", "All fields are required");
+      } else {
+        setShowLoader(true);
+        handleFlutterPayment({
+          callback: (response) => {
+            console.log(response);
+            setShowLoader(false);
+            if (response.status !== "successful") {
+              toastMessage("Error", "Transaction failed!");
+            } else {
+              closePaymentModal();
+              toastMessage("success", "Transaction paid successfull!");
+              // setShowLoader(true);
+              // axi.post(
+              //   process.env.REACT_APP_BACKEND_URL + "/cart/completedOrder2/",
+              //   {
+              //     i: item._id,
+              //     totalAmount: response.amount,
+              //     transactionId: response.transaction_id,
+              //     token,
+              //   }
+              // )
+              //   .then((res) => {
+              //     toastMessage("success", res.data.msg);
+              //     navigate("/profile/completedOrders");
+              //     window.location.reload();
+              //   })
+              //   .catch((error) => {
+              //     setShowLoader(false);
+              //     errorHandler(error);
+              //   });
+            }
+          },
+          onClose: () => {
+            console.log("closed...");
+            setShowLoader(false);
+          },
+        });
+      }
+    }
+  };
+
   return (
     <>
       <div className="headerr">
@@ -96,6 +170,8 @@ function Exchange() {
           <div className="my-3">
             <input
               className="form-control"
+              value={phoneOrAccount}
+              onChange={(e) => setPhoneOrAccount(e.target.value)}
               placeholder="Enter account number or phone number for us to transfer the exchange"
             />
           </div>
@@ -104,11 +180,14 @@ function Exchange() {
             to the account number of phone number specified.
           </small>
           <div>
-            <button className="btn">PAY NOW</button>
+            <button className="btn" onClick={() => handlePay()}>
+              PAY NOW
+            </button>
           </div>
         </div>
       </div>
       <Footer />
+      <Loader showLoader={showLoader} />
     </>
   );
 }
